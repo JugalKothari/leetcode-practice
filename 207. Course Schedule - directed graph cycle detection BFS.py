142690283f8bfe6b07ee4c178adbf806f024b26f
Jugal Kothari <93007069+JugalKothari@users.noreply.github.com>
2024-08-19 17:32:33 +0530

    Update and rename 207. Course Schedule - directed graph cycle detection.py to 207. Course Schedule - directed graph cycle detection BFS.py

--- 207. Course Schedule - directed graph cycle detection BFS.py
+++ 207. Course Schedule - directed graph cycle detection BFS.py
@@ -1,25 +1,22 @@
 from queue import Queue
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        visited=[]
-        adj={i:[] for i in range(numCourses)}
+        adj = {i:[] for i in range(numCourses)}
         indegrees=[0]*numCourses
-        for course, pre in prerequisites:
-            adj[pre].append(course)
-            indegrees[course]+=1
+        for course, prereq in prerequisites:
+            adj[course].append(prereq)
+            indegrees[prereq]+=1
         q=Queue()
         for i in range(numCourses):
-            if indegrees[i]==0:
+            if indegrees[i] == 0:
                 q.put(i)
-        
         processed_courses=0
         while not q.empty():
             course=q.get()
             processed_courses+=1
-            for dep in adj[course]:
-                indegrees[dep]-=1
-                if indegrees[dep]==0:
-                    q.put(dep)
-                    
+            for prereq in adj[course]:
+                indegrees[prereq]-=1
+                if indegrees[prereq]==0:
+                    q.put(prereq)
         return processed_courses==numCourses
       
