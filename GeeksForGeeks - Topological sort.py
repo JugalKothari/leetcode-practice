18a2b7b1134220f5117de0e402b935e38be0d828
Jugal Kothari <93007069+JugalKothari@users.noreply.github.com>
2024-08-20 16:20:51 +0530

    Update GeeksForGeeks - Topological sort.py

--- GeeksForGeeks - Topological sort.py
+++ GeeksForGeeks - Topological sort.py
@@ -1,3 +1,4 @@
+#Solution using dfs approach
 class Solution:
     
     #Function to return list containing vertices in Topological order.
@@ -14,5 +15,36 @@ class Solution:
         for starting in range(V):
             if not visited[starting]:
                 dfs(starting, 0)
+        
+        return order
+
+#Solution using bfs approach
+from collections import deque
+class Solution:
+    
+    #Function to return list containing vertices in Topological order.
+    def topoSort(self, V, adj):
+        visited=[False]*V
+        order=[]
+        indegrees=[0]*V
+        for node in range(V):
+            for adjacent in adj[node]:
+                indegrees[adjacent]+=1
+        
+        q=deque()
+        order=[]
+        for node in range(V):
+            if indegrees[node]==0:
+                q.append(node)
+                order.append(node)
+                
+        while q:
+            node=q.popleft()
+            for adjacent in adj[node]:
+                indegrees[adjacent]-=1
+                
+                if indegrees[adjacent]==0:
+                    q.append(adjacent)
+                    order.append(adjacent)
         
         return order
