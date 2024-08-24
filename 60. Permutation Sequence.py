cf0ad667cdbbb686aea5c69f4bb2b2c2e7039853
Jugal Kothari <93007069+JugalKothari@users.noreply.github.com>
2024-08-24 19:18:38 +0530

    Update 60. Permutation Sequence.py

--- 60. Permutation Sequence.py
+++ 60. Permutation Sequence.py
@@ -7,4 +7,24 @@ class Solution:
         perms=["".join(p) for p in result]
         perms.sort()
         return perms[k-1]
-        
+
+#Math based solution
+from itertools import permutations
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        numbers=[]
+        fact=1
+        for i in range(1, n):
+            numbers.append(i)
+            fact*=i
+        numbers.append(n)
+        ans=""
+        k-=1
+        while True:
+            ans+=str(numbers[k//fact])
+            numbers.pop(k//fact)
+            if not numbers:
+                break
+            k=k%fact
+            fact=fact//len(numbers)
+        return ans      
