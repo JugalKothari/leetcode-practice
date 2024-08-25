281dcefe0e6c9eb0df08cbc159b6f1dac8b70402
Jugal Kothari <93007069+JugalKothari@users.noreply.github.com>
2024-08-25 14:12:21 +0530

    Create GeeksForGeeks - Implement stack using array.py

---
+++ GeeksForGeeks - Implement stack using array.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+
+class MyStack:
+    
+    def __init__(self):
+        self.arr=[]
+    
+    #Function to push an integer into the stack.
+    def push(self,data):
+        self.arr.append(data)
+    
+    #Function to remove an item from top of the stack.
+    def pop(self):
+        if len(self.arr)>0:
+            return self.arr.pop()
+        return -1
